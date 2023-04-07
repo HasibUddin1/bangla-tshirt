a33acdd32204217a3554e16382f7f29d638f9c74
IconicLuck <uhasib959@gmail.com>
2023-04-07 20:38:44 +0600

    implemented order summery on home page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({cart,removeFromCart}) => {
     return (
         <div>
-            <h1 className='text-4xl font-bold'>Order Summery: </h1>
+            <h1 className='text-4xl font-bold'>Order Summery</h1>
+            <h2 className='text-3xl font-semibold'>Selected Items: {cart.length}</h2>
+            {
+                cart.map(tshirt => <p
+                    className='text-xl font-semibold mb-3'
+                    key={tshirt._id}
+                >{tshirt.name} <button onClick={() => removeFromCart(tshirt._id)} className='bg-slate-500 hover:bg-slate-700 ease-in-out duration-200 text-white p-2 rounded-lg'>X</button></p>)
+            }
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,14 +3,29 @@ import { useLoaderData } from 'react-router-dom';
 import TShirt from '../TShirt/TShirt';
 import Cart from '../Cart/Cart';
 import './Home.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Home = () => {
     const tshirts = useLoaderData()
 
-    const [] = useState([])
+    const [cart, setCart] = useState([])
 
     const handleAddToCart = tshirt => {
-        console.log(tshirt)
+        const exists = cart.find(ts => ts._id === tshirt._id)
+        if (exists) {
+            toast('You have already selected this item')
+        }
+        else {
+            const newCart = [...cart, tshirt]
+            setCart(newCart)
+        }
+    }
+
+    const removeFromCart = id => {
+        const remaining = cart.filter(tshirt => tshirt._id !== id)
+        setCart(remaining)
     }
 
     return (
@@ -25,7 +40,10 @@ const Home = () => {
                 }
             </div>
             <div className='mt-10'>
-                <Cart></Cart>
+                <Cart
+                    cart={cart}
+                    removeFromCart={removeFromCart}
+                ></Cart>
             </div>
         </div>
     );
